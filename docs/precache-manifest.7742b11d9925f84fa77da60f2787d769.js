02dd536e68b67b0d84383135187b3f970b7e46eb
Bastian Ortega <bastianort@gmail.com>
2020-07-17 21:59:19 -0400

    docs agregado

---
+++ docs/precache-manifest.7742b11d9925f84fa77da60f2787d769.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54aa857818e87e647680e1e7a41fa630",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbbd87d052e047560d39",
+    "url": "/static/css/main.46f7fd2c.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbbd87d052e047560d39",
+    "url": "/static/js/main.76f283c5.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
